183a6fa99d51fb4492599441ce6526905115c135
hidaehyunlee <hidaehyunlee@gmail.com>
2023-09-05 21:31:44 +0900

    Feat: addMarker and setInitialCameraPosition

--- Sojumap/Controller/MapViewController.swift
+++ Sojumap/Controller/MapViewController.swift
@@ -12,14 +12,17 @@ import Alamofire
 import SwiftyJSON
 import SwiftSoup
 
-class MapViewController: UIViewController {
-
+class MapViewController: UIViewController, NMFMapViewDelegate {
+    @IBOutlet weak var naverMapView: NMFNaverMapView!
     let NAVER_GEOCODE_URL = "https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query="
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let address = "서울특별시 강남구 강남대로 364"
+        // NMFNaverMapView delegate 설정
+        naverMapView.mapView.delegate = self
+        
+        let address = "서울 중구 남대문로1길 11"
         let encodedAddress = address.addingPercentEncoding( withAllowedCharacters: NSCharacterSet.urlQueryAllowed)
         
         convertAddressToCoordinate(address: encodedAddress)
@@ -38,15 +41,36 @@ class MapViewController: UIViewController {
                     let json = JSON(value)
                     let data = json["addresses"]
                     
-                    let lat = data[0]["y"]
-                    let lon = data[0]["x"]
+                    let lat = data[0]["y"].doubleValue
+                    let lon = data[0]["x"].doubleValue
                     let roadAddr = data[0]["roadAddress"]
+                    let coordinate = NMGLatLng(lat: lat, lng: lon)
+                    
                     print("위도:", lat, "경도:", lon, "도로명주소:", roadAddr)
+                    
+                    // 초기 카메라 위치를 마커가 있는 위치로 설정
+                    self.addMarker(at: coordinate)
+                    self.setInitialCameraPosition(at: coordinate)
+                    
                 case .failure(let error):
                     print(error.errorDescription ?? "")
                 default :
                     fatalError()
                 }
             }
+    }
+    
+    // 마커 추가 함수
+    func addMarker(at latlng: NMGLatLng) {
+        let marker = NMFMarker() // !! 할일: 상속 받는 새 마커 클래스 만들기
+        
+        marker.position = latlng
+        marker.mapView = naverMapView.mapView
+    }
+    
+    // 초기 카메라 위치 설정 함수
+    func setInitialCameraPosition(at latlng: NMGLatLng) {
+        naverMapView.mapView.positionMode = .disabled
+        naverMapView.mapView.moveCamera(NMFCameraUpdate(scrollTo: latlng))
     }
 }
