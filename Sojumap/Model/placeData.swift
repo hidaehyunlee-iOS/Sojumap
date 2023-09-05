f74fe6e5b53447742569dcec674e50976543e63e
kimhr <khl9566@naver.com>
2023-09-05 11:06:01 +0900

    commit place data 클래스 생성

---
+++ Sojumap/Model/placeData.swift
@@ -0,0 +1,22 @@
+//
+//  placeData.swift
+//  Sojumap
+//
+//  Created by t2023-m0067 on 2023/09/05.
+//
+
+import Foundation
+
+class placeData {
+    let name: String // 장소이름
+    let url: String
+    let latitude: Double // 위도
+    let longitude: Double // 경도
+    
+    init(name: String, url: String, latitude: Double, longitude: Double) {
+        self.name = name
+        self.url = url
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+}
