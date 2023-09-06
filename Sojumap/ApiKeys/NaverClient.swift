068651c17dd341aca199d0715f58d4dd88fa01ea
hidaehyunlee <hidaehyunlee@gmail.com>
2023-09-06 16:57:10 +0900

    add API KEY

---
+++ Sojumap/ApiKeys/NaverClient.swift
@@ -0,0 +1,11 @@
+//
+//  NaverClient.swift
+//  Sojumap
+//
+//  Created by daelee on 2023/09/05.
+//
+
+import Foundation
+
+let NAVER_CLIENT_ID = "gb3gl91o97"
+let NAVER_CLIENT_SECRET = "iBegySENzfu0n5mVoyJuhUELc9x6pGpDhlt2IY8Y"
