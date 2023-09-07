5c018c6753b4b3b5dcea449f6548373a8731fbbb
hidaehyunlee <hidaehyunlee@gmail.com>
2023-09-07 13:13:30 +0900

    Feat: maker touchHandler

--- Sojumap/Controller/MapViewController.swift
+++ Sojumap/Controller/MapViewController.swift
@@ -16,15 +16,18 @@ class CustomMarker: NMFMarker {
     var title: String?
     var name: String?
     var address: String?
+    var customUserInfo: [String: Any]?
 
-    init(position: NMGLatLng, title: String?, name: String?, address: String?) {
+    init(position: NMGLatLng, title: String?, name: String?, address: String?, customUserInfo: [String: Any]? = nil) {
         super.init()
         self.position = position
         self.title = title
         self.name = name
         self.address = address
+        self.customUserInfo = customUserInfo
     }
 }
+
 
 class MapViewController: UIViewController, NMFMapViewDelegate {
     @IBOutlet weak var naverMapView: NMFNaverMapView!
@@ -32,9 +35,10 @@ class MapViewController: UIViewController, NMFMapViewDelegate {
     @IBOutlet weak var placeAddr: UILabel!
     
     let NAVER_GEOCODE_URL = "https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query="
-    // let customModalView = CustomModalView() // CustomModalView 인스턴스를 생성
     var initialMarkerName: String?
     var initialMarkerAddress: String?
+    var allMarkers: [CustomMarker] = [] // addMaker()에서 추가
+    var count = 1 // 각 마커를 tag로 구분하기 위한 카운트 변수
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,6 +48,9 @@ class MapViewController: UIViewController, NMFMapViewDelegate {
     }
     
     private func setMap() {
+        naverMapView.showLocationButton = true // 현재위치 버튼
+        naverMapView.mapView.zoomLevel = 11 // 값이 클수록 지도 확대
+
         // 비디오 데이터 처리
         for dummyData in dummyDataList {
             guard let title = dummyData["Title"] as? String,
@@ -90,11 +97,30 @@ class MapViewController: UIViewController, NMFMapViewDelegate {
     
     // 마커 추가 함수
     func addMarker(at latlng: NMGLatLng, title: String?, name: String?, address: String?) {
-        let marker = CustomMarker(position: latlng, title: title, name: name, address: address)
+        let marker = CustomMarker(position: latlng, title: title, name: name, address: address, customUserInfo: ["tag" : count])
         
+        count += 1
+        allMarkers.append(marker)
+        
+        for marker in allMarkers {
+            marker.touchHandler = { (overlay) -> Bool in
+                if let customMarker = overlay as? CustomMarker,
+                   let tag = customMarker.customUserInfo?["tag"] as? Int {
+                    print("마커 \(tag) 터치됨")
+                    
+                    // 마커를 클릭했을 때 placeName.text와 placeAddr.text를 업데이트
+                    self.placeName.text = customMarker.name
+                    self.placeAddr.text = customMarker.address
+                }
+                return false
+            }
+        }
+
         marker.mapView = naverMapView.mapView
+        marker.captionRequestedWidth = 60 // 캡션 너비
+        marker.captionText = name ?? "" // 캡션 네임
         
-        // 제일 처음 생성된 마커 정보만 표시
+        // 하단 뷰에는 제일 처음 생성된 마커 정보만 표시
         if initialMarkerName == nil && initialMarkerAddress == nil {
             initialMarkerName = name
             initialMarkerAddress = address
