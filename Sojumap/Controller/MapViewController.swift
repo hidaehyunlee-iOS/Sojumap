4101815ef5142b6ce03b5cf846283264e6956941
hidaehyunlee <hidaehyunlee@gmail.com>
2023-09-08 13:41:25 +0900

    Fix: distanceKM UI

--- Sojumap/Controller/MapViewController.swift
+++ Sojumap/Controller/MapViewController.swift
@@ -26,10 +26,8 @@ class MapViewController: UIViewController, NMFMapViewDelegate, CLLocationManager
     @IBAction func showListButton(_ sender: UIBarButtonItem) {
         let storyBoard = UIStoryboard(name: "MapTableViewController", bundle: nil)
         let vc = storyBoard.instantiateViewController(withIdentifier: "MapTableViewController") as! MapTableViewController
-        //self.present(vc, animated: false, completion: nil)
         
         self.navigationController?.pushViewController(vc, animated: true)
-        // self.present(MapTableVC, animated: false, completion: nil)
     }
     
     @IBAction func detailPageButton(_ sender: UIButton) {
@@ -44,113 +42,136 @@ class MapViewController: UIViewController, NMFMapViewDelegate, CLLocationManager
     }
     
     override func viewDidLoad() {
-            super.viewDidLoad()
-            setMap()
-            
-            naverMapView?.mapView.delegate = self
-            
-            locationManager.delegate = self
-            locationManager.desiredAccuracy = kCLLocationAccuracyBest
-            locationManager.requestWhenInUseAuthorization()
-            locationManager.startUpdatingLocation()
-        }
+        super.viewDidLoad()
         
-        private func setMap() {
-            naverMapView?.showLocationButton = true
-            naverMapView?.mapView.zoomLevel = 11
-            
-            for dummyData in dummyDataList {
-                guard let title = dummyData["Title"] as? String,
-                      let descriptions = dummyData["Description"] as? [String], descriptions.count >= 2,
-                      let name = descriptions.first,
-                      let address = descriptions.last,
-                      let encodedAddress = address.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
-                    continue
-                }
-                
-                convertAddressToCoordinate(title: title, name: name, address: encodedAddress)
-            }
-        }
+        configMap()
+        configLocation()
+    }
+    
+    private func configMap() {
+        naverMapView?.mapView.delegate = self
+        naverMapView?.showLocationButton = true
+        naverMapView?.mapView.zoomLevel = 11
         
-        func convertAddressToCoordinate(title: String?, name: String?, address: String?) {
-            let header1 = HTTPHeader(name: "X-NCP-APIGW-API-KEY-ID", value: NAVER_CLIENT_ID)
-            let header2 = HTTPHeader(name: "X-NCP-APIGW-API-KEY", value: NAVER_CLIENT_SECRET)
-            let headers = HTTPHeaders([header1, header2])
-            
-            AF.request(NAVER_GEOCODE_URL + address!, method: .get, encoding: URLEncoding.default, headers: headers).validate()
-                .responseJSON { response in
-                    switch response.result {
-                    case .success(let value as [String: Any]):
-                        let json = JSON(value)
-                        let data = json["addresses"]
-                        
-                        let lat = data[0]["y"].doubleValue
-                        let lon = data[0]["x"].doubleValue
-                        let roadAddr = data[0]["roadAddress"].stringValue
-                        let coordinate = NMGLatLng(lat: lat, lng: lon)
-                        
-                        self.addMarker(at: coordinate, title: title, name: name, address: roadAddr)
-                        
-                    case .failure(let error):
-                        print(error.errorDescription ?? "")
-                    default:
-                        fatalError()
-                    }
-                }
-        }
-        
-        func addMarker(at latlng: NMGLatLng, title: String?, name: String?, address: String?) {
-            let marker = CustomMarker(position: latlng, title: title, name: name, address: address, distance: nil, customUserInfo: ["tag": markerCount])
-            
-            markerCount += 1
-            allMarkers.append(marker)
-            
-            for marker in allMarkers {
-                marker.touchHandler = { (overlay) -> Bool in
-                    if let customMarker = overlay as? CustomMarker,
-                       let tag = customMarker.customUserInfo?["tag"] as? Int {
-                        self.placeName?.text = customMarker.name
-                        self.placeAddr?.text = customMarker.address
-                        self.calculateDistance(marker: customMarker)
-                    }
-                    return false
-                }
+        for dummyData in dummyDataList {
+            guard let title = dummyData["Title"] as? String,
+                  let descriptions = dummyData["Description"] as? [String], descriptions.count >= 2,
+                  let name = descriptions.first,
+                  let address = descriptions.last,
+                  let encodedAddress = address.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+                continue
             }
             
-            marker.mapView = naverMapView?.mapView
-            marker.captionRequestedWidth = 60
-            marker.captionText = name ?? ""
-            
-            if initialMarkerName == nil && initialMarkerAddress == nil {
-                initialMarkerName = name
-                initialMarkerAddress = address
-                
-                placeName?.text = marker.name
-                placeAddr?.text = marker.address
-                
-                setInitialCameraPosition(at: latlng)
-                calculateDistance(marker: marker)
-            }
-        }
-        
-        func calculateDistance(marker: CustomMarker) {
-            guard let currentLocation = locationManager.location else {
-                return
-            }
-            
-            print(currentLocation.coordinate.latitude)
-            print(currentLocation.coordinate.longitude)
-
-            
-            let markerLocation = CLLocation(latitude: marker.position.lat, longitude: marker.position.lng)
-            let distanceM = currentLocation.distance(from: markerLocation)
-            let distanceKM = distanceM / 1000.0
-            
-            distanceInKilometers?.text = String(format: "%.2f km", distanceKM)
-        }
-        
-        func setInitialCameraPosition(at latlng: NMGLatLng) {
-            naverMapView?.mapView.positionMode = .disabled
-            naverMapView?.mapView.moveCamera(NMFCameraUpdate(scrollTo: latlng))
+            convertAddressToCoordinate(title: title, name: name, address: encodedAddress)
         }
     }
+    
+    private func configLocation() {
+        locationManager.delegate = self // 델리게이트 설정
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest // 거리 정확도 설정
+        locationManager.requestWhenInUseAuthorization() // 사용자에게 허용 받기 alert 띄우기
+
+        // 위치 사용을 허용하면 현재 위치 정보를 가져옴
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+        }
+        else {
+            print("위치 서비스 허용 off")
+        }
+    }
+    
+    func convertAddressToCoordinate(title: String?, name: String?, address: String?) {
+        let header1 = HTTPHeader(name: "X-NCP-APIGW-API-KEY-ID", value: NAVER_CLIENT_ID)
+        let header2 = HTTPHeader(name: "X-NCP-APIGW-API-KEY", value: NAVER_CLIENT_SECRET)
+        let headers = HTTPHeaders([header1, header2])
+        
+        AF.request(NAVER_GEOCODE_URL + address!, method: .get, encoding: URLEncoding.default, headers: headers).validate()
+            .responseJSON { response in
+                switch response.result {
+                case .success(let value as [String: Any]):
+                    let json = JSON(value)
+                    let data = json["addresses"]
+                    
+                    let lat = data[0]["y"].doubleValue
+                    let lon = data[0]["x"].doubleValue
+                    let roadAddr = data[0]["roadAddress"].stringValue
+                    let coordinate = NMGLatLng(lat: lat, lng: lon)
+                    
+                    self.setMarkers(at: coordinate, title: title, name: name, address: roadAddr)
+                    
+                case .failure(let error):
+                    print(error.errorDescription ?? "")
+                default:
+                    fatalError()
+                }
+            }
+    }
+    
+    func setInitialCameraPosition(at latlng: NMGLatLng) {
+        naverMapView?.mapView.positionMode = .disabled
+        naverMapView?.mapView.moveCamera(NMFCameraUpdate(scrollTo: latlng))
+    }
+    
+    func setMarkers(at latlng: NMGLatLng, title: String?, name: String?, address: String?) {
+        let marker = CustomMarker(position: latlng, title: title, name: name, address: address, distanceKM: nil, customUserInfo: ["tag": markerCount])
+        
+        markerCount += 1
+        allMarkers.append(marker)
+        
+        for marker in allMarkers {
+            marker.touchHandler = { (overlay) -> Bool in
+                if let customMarker = overlay as? CustomMarker,
+                   let tag = customMarker.customUserInfo?["tag"] as? Int {
+                    self.placeName?.text = customMarker.name
+                    self.placeAddr?.text = customMarker.address
+                    self.calculateAndSetDistance(marker: customMarker)
+                }
+                return false
+            }
+        }
+        
+        marker.mapView = naverMapView?.mapView
+        marker.captionRequestedWidth = 60
+        marker.captionText = name ?? ""
+        
+        if initialMarkerName == nil && initialMarkerAddress == nil {
+            initialMarkerName = name
+            initialMarkerAddress = address
+            
+            placeName?.text = marker.name
+            placeAddr?.text = marker.address
+            
+            setInitialCameraPosition(at: latlng)
+            calculateAndSetDistance(marker: marker)
+        }
+    }
+    
+    func calculateAndSetDistance(marker: CustomMarker) {
+        guard let currentLocation = locationManager.location else {
+            return
+        }
+        
+        print(currentLocation.coordinate.latitude)
+        print(currentLocation.coordinate.longitude)
+        
+        let markerLocation = CLLocation(latitude: marker.position.lat, longitude: marker.position.lng)
+        let distanceM = currentLocation.distance(from: markerLocation)
+        marker.distanceKM = distanceM / 1000.0
+        
+        distanceInKilometers?.text = String(format: "%.2f km", marker.distanceKM!)
+    }
+    
+// 위치 정보 계속 업데이트 -> 위도 경도 받아옴
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        print("위치 업데이트")
+//        if let location = locations.first {
+//            print("위도: \(location.coordinate.latitude)")
+//            print("경도: \(location.coordinate.longitude)")
+//        }
+//    }
+    
+    // 위치 가져오기 실패
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+}
--- Sojumap/Model/CustonMarker.swift
+++ Sojumap/Model/CustonMarker.swift
@@ -12,16 +12,16 @@ class CustomMarker: NMFMarker {
     var title: String?
     var name: String?
     var address: String?
-    var distance: Double?
+    var distanceKM: Double?
     var customUserInfo: [String: Any]?
 
-    init(position: NMGLatLng, title: String?, name: String?, address: String?, distance: Double? ,customUserInfo: [String: Any]? = nil) {
+    init(position: NMGLatLng, title: String?, name: String?, address: String?, distanceKM: Double? ,customUserInfo: [String: Any]? = nil) {
         super.init()
         self.position = position
         self.title = title
         self.name = name
         self.address = address
-        self.distance = distance
+        self.distanceKM = distanceKM
         self.customUserInfo = customUserInfo
     }
 }
