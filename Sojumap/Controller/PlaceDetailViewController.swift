a08dcd2e9cb10d170e9bb26e5facde8ef104e17d
kimhr <khl9566@naver.com>
2023-09-10 17:02:39 +0900

    Style : 식당 정보와 지도 위치 변경

--- Sojumap/Controller/PlaceDetailViewController.swift
+++ Sojumap/Controller/PlaceDetailViewController.swift
@@ -14,13 +14,14 @@ import Alamofire
 import SwiftyJSON
 
 class PlaceDetailViewController: UIViewController {
+    @IBOutlet weak var scrollView: UIScrollView!
     // 영상 재생 view
     @IBOutlet weak var playerView: WKWebView?
   
     @IBOutlet weak var placeInformView: UIStackView?
     @IBOutlet weak var expandButton: UIButton!
     @IBOutlet weak var mapView: NMFNaverMapView?
-    @IBOutlet weak var secondViewBottomConstraint: NSLayoutConstraint! // 두 번째 UIView의 하단 제약
+//    @IBOutlet weak var secondViewBottomConstraint: NSLayoutConstraint! // 두 번째 UIView의 하단 제약
     
     var isExpanded = true // 확장 상태를 추적하는 변수
     
@@ -44,9 +45,9 @@ class PlaceDetailViewController: UIViewController {
         
         // 디테일 페이지로 넘어올 때 full screen으로 보여지게 작업(메인 작업 완료 후 작업)
 //        UIModalPresentationStyle.fullScreen
-        
+//        scrollView.contentSize = CGSize(width: scrollView.frame.size.width, height: 100)
         // 스택뷰 초기 상태 설정
-        placeInformView?.isHidden = true
+        placeInformView?.isHidden = false
         // ui 스타일 지정
         setUpStyle()
         // 데이터 받아오기
@@ -62,22 +63,22 @@ class PlaceDetailViewController: UIViewController {
     // 더보기 버튼
     @IBAction func toggleStackView(_ sender: UIButton) {
         // 첫 번째 Stack View의 숨김 상태 토글
-        placeInformView?.isHidden = !isExpanded
-
-        // 스택 뷰가 펼쳐져 있는지 확인하고 애니메이션으로 확장 또는 축소
-        UIView.animate(withDuration: 0.3) { [weak self] in
-            if let isExpanded = self?.isExpanded {
-                // 첫 번째 Stack View가 숨겨져 있는 경우 두 번째 UIView를 아래로 이동
-                self?.secondViewBottomConstraint.constant = isExpanded ? 0 : (self?.placeInformView?.frame.height ?? 0)
-                self?.view.layoutIfNeeded()
-            }
-        }
-
-        // 확장 상태 업데이트
-        isExpanded = !isExpanded
-
-        // 버튼 이미지 변경
-        isExpanded ? expandButton.setImage(UIImage(systemName: "chevron.down"), for: .normal) : expandButton.setImage(UIImage(systemName: "chevron.up"), for: .normal)
+//        placeInformView?.isHidden = !isExpanded
+//
+//        // 스택 뷰가 펼쳐져 있는지 확인하고 애니메이션으로 확장 또는 축소
+//        UIView.animate(withDuration: 0.3) { [weak self] in
+//            if let isExpanded = self?.isExpanded {
+//                // 첫 번째 Stack View가 숨겨져 있는 경우 두 번째 UIView를 아래로 이동
+//                self?.secondViewBottomConstraint.constant = isExpanded ? 0 : (self?.placeInformView?.frame.height ?? 0)
+//                self?.view.layoutIfNeeded()
+//            }
+//        }
+//
+//        // 확장 상태 업데이트
+//        isExpanded = !isExpanded
+//
+//        // 버튼 이미지 변경
+//        isExpanded ? expandButton.setImage(UIImage(systemName: "chevron.down"), for: .normal) : expandButton.setImage(UIImage(systemName: "chevron.up"), for: .normal)
 
     }
     
@@ -97,6 +98,7 @@ class PlaceDetailViewController: UIViewController {
               let dataID = data.videoId,
               let dataTitle = data.title,
               let viewCount = data.releaseViewCount,
+//              let hashtag = data.hashtags[],
               let name = data.videoInfo[safe: 0] ?? "",
               let addr = data.videoInfo[safe: 1] ?? ""
         else {return}
